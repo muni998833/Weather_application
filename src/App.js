29f0e47d20352cb80eb2b39d222ce19473391e2d
muni998833 <muni998833@gmail.com>
2025-02-06 08:23:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import WheatherApp from './components/Wheather App/WheatherApp';
+
+function App() {
+  return (
+    <div className="App">
+      <WheatherApp></WheatherApp>
+    </div>
+  );
+}
+
+export default App;
